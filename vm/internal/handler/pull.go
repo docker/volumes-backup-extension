689a9c222b480db0d04be1fbc881bfdd2f5a11d4
felipecruz91 <felipecruz91@hotmail.es>
2022-08-12 08:45:16 +0200

    Add tests about pulling volumes with and without auth

--- vm/internal/handler/pull.go
+++ vm/internal/handler/pull.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"encoding/json"
 	"github.com/docker/distribution/reference"
 	dockertypes "github.com/docker/docker/api/types"
 	"github.com/felipecruz91/vackup-docker-extension/internal/backend"
@@ -17,15 +16,10 @@ type PullRequest struct {
 	Reference string `json:"reference"`
 }
 
-type PullErrorLine struct {
-	ErrorDetail ErrorDetail `json:"errorDetail"`
-	Error       string      `json:"error"`
-}
-
 // PullVolume pulls a volume from a registry.
 // The user must be previously authenticated to the registry with `docker login <registry>`, otherwise it returns 401 StatusUnauthorized.
 func (h *Handler) PullVolume(ctx echo.Context) error {
-	var request PushRequest
+	var request PullRequest
 	if err := ctx.Bind(&request); err != nil {
 		return err
 	}
@@ -34,7 +28,7 @@ func (h *Handler) PullVolume(ctx echo.Context) error {
 	volumeName := ctx.Param("volume")
 	log.Infof("volumeName: %s", volumeName)
 	log.Infof("reference: %s", request.Reference)
-	logrus.Infof("received push request for volume %s\n", volumeName)
+	logrus.Infof("received pull request for volume %s\n", volumeName)
 
 	if volumeName == "" {
 		return ctx.String(http.StatusBadRequest, "volume is required")
@@ -46,46 +40,39 @@ func (h *Handler) PullVolume(ctx echo.Context) error {
 	}
 	log.Infof("parsedRef.String(): %s", parsedRef.String())
 
-	// Push the image to registry
+	// Pull the volume (image) from registry
 	encodedAuth := ctx.Request().Header.Get("X-Registry-Auth")
 	if encodedAuth == "" {
 		encodedAuth = "Cg==" // from running: echo "" | base64
 	}
+
+	log.Infof("Pulling image %s...", parsedRef.String())
 	pullResp, err := h.DockerClient.ImagePull(ctxReq, parsedRef.String(), dockertypes.ImagePullOptions{
 		RegistryAuth: encodedAuth,
 	})
+
 	if err != nil {
 		log.Error(err)
+
+		if strings.Contains(err.Error(), "unauthorized: authentication required") {
+			return ctx.String(http.StatusUnauthorized, err.Error())
+		}
+
 		return ctx.String(http.StatusInternalServerError, err.Error())
 	}
 	defer pullResp.Close()
 
-	response, err := ioutil.ReadAll(pullResp)
+	pullRespBytes, err := ioutil.ReadAll(pullResp)
+	if err != nil {
+		return ctx.String(http.StatusInternalServerError, err.Error())
+	}
 
-	for _, line := range strings.Split(string(response), "\n") {
+	for _, line := range strings.Split(string(pullRespBytes), "\n") {
 		log.Info(line)
-
-		if !strings.Contains(line, "error") {
-			continue
-		}
-
-		pel := PushErrorLine{}
-		if err := json.Unmarshal([]byte(line), &pel); err == nil {
-			// TODO: double check
-			// the image pull had an error, e.g:
-			// {"errorDetail":{"message":"unauthorized: authentication required"},"error":"unauthorized: authentication required"}
-			// or
-			// {"errorDetail":{"message":"no basic auth credentials"},"error":"no basic auth credentials"}
-			log.Error(err)
-			if pel.Error == "unauthorized: authentication required" || pel.Error == "no basic auth credentials" {
-				return ctx.String(http.StatusUnauthorized, pel.Error)
-			} else {
-				return ctx.String(http.StatusInternalServerError, pel.Error)
-			}
-		}
 	}
 
 	// Load the image into the volume
+	log.Infof("Loading image %s into volume %s...", parsedRef.String(), volumeName)
 	if err := backend.Load(ctxReq, h.DockerClient, volumeName, parsedRef.String()); err != nil {
 		log.Error(err)
 		return ctx.String(http.StatusInternalServerError, err.Error())
--- vm/internal/handler/pull_test.go
+++ vm/internal/handler/pull_test.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/base64"
 	"fmt"
 	"github.com/docker/docker/api/types"
 	dockertypes "github.com/docker/docker/api/types"
@@ -46,6 +48,18 @@ func TestPullVolume(t *testing.T) {
 			t.Log(err)
 		}
 	}()
+
+	// Setup
+	e := echo.New()
+	requestJSON := fmt.Sprintf(`{"reference": "%s"}`, imageID)
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(requestJSON))
+	req.Header.Add("Content-Type", "application/json")
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/volumes/:volume/pull")
+	c.SetParamNames("volume")
+	c.SetParamValues(volume)
+	h := New(c.Request().Context(), setupDockerClient(t))
 
 	// Provision a registry with an image (which represents a volume) ready to pull:
 	// Run a local registry
@@ -92,19 +106,7 @@ func TestPullVolume(t *testing.T) {
 	}
 	defer resp.Body.Close()
 
-	// Setup
-	e := echo.New()
-	requestJSON := fmt.Sprintf(`{"reference": "%s"}`, imageID)
-	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(requestJSON))
-	req.Header.Add("Content-Type", "application/json")
-	rec := httptest.NewRecorder()
-	c := e.NewContext(req, rec)
-	c.SetPath("/volumes/:volume/pull")
-	c.SetParamNames("volume")
-	c.SetParamValues(volume)
-	h := New(c.Request().Context(), setupDockerClient(t))
-
-	//Push volume to registry.
+	// Push volume to registry.
 	// The registry container state may be "running" but not ready to receive requests when reaching this line.
 	err = retry(10, 1*time.Second, func() error {
 		pushResp, err := cli.ImagePush(context.Background(), imageID, dockertypes.ImagePushOptions{
@@ -128,25 +130,12 @@ func TestPullVolume(t *testing.T) {
 
 		return nil
 	})
-
 	if err != nil {
 		t.Fatal(err)
 	}
-	//for {
-	//	containers, err := cli.ContainerList(c.Request().Context(), types.ContainerListOptions{
-	//		Quiet:   false,
-	//		All:     true,
-	//		Limit:   1,
-	//		Filters: filters.NewArgs(filters.Arg("id", registryContainerID)),
-	//	})
-	//	if err != nil {
-	//		t.Fatal(err)
-	//	}
-	//
-	//	if containers[0].State == "running" {
-	//		break
-	//	}
-	//}
+
+	// The sleep is to ensure the image is present in the registry after the `ImagePush` operation.
+	time.Sleep(3 * time.Second)
 
 	_, err = cli.ImageRemove(context.Background(), imageID, types.ImageRemoveOptions{
 		Force: true,
@@ -174,7 +163,292 @@ func TestPullVolume(t *testing.T) {
 	m := backend.GetVolumesSize(c.Request().Context(), h.DockerClient, volume)
 	require.Equal(t, int64(16000), m[volume].Bytes)
 	require.Equal(t, "16.0 kB", m[volume].Human)
+}
 
+func TestPullVolumeUsingCorrectAuth(t *testing.T) {
+	var containerID string
+	var registryContainerID string
+	volume := "b3128131acca4d70263d477345b528ad3aba3ae66f2d94dddb02817da427020a"
+	registry := "localhost:5000" // or use docker.io to push it to DockerHub
+	imageID := registry + "/felipecruz/" + "vackup-pull-test-img"
+	cli := setupDockerClient(t)
+
+	defer func() {
+		_ = cli.ContainerRemove(context.Background(), registryContainerID, types.ContainerRemoveOptions{
+			Force: true,
+		})
+		_ = cli.ContainerRemove(context.Background(), containerID, types.ContainerRemoveOptions{
+			Force: true,
+		})
+		_ = cli.VolumeRemove(context.Background(), volume, true)
+
+		t.Logf("removing image %s", imageID)
+		if _, err := cli.ImageRemove(context.Background(), imageID, types.ImageRemoveOptions{
+			Force: true,
+		}); err != nil {
+			t.Log(err)
+		}
+	}()
+
+	auth := `{"username": "testuser", "password": "testpassword"}`
+	encodedAuth := base64.StdEncoding.EncodeToString([]byte(auth))
+
+	// Setup
+	e := echo.New()
+	requestJSON := fmt.Sprintf(`{"reference": "%s"}`, imageID)
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(requestJSON))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-Registry-Auth", encodedAuth)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/volumes/:volume/pull")
+	c.SetParamNames("volume")
+	c.SetParamValues(volume)
+	h := New(c.Request().Context(), setupDockerClient(t))
+
+	// Run a local registry with auth
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp2, err := cli.ContainerCreate(context.Background(), &container.Config{
+		Image: "docker.io/library/registry:2",
+		ExposedPorts: map[nat.Port]struct{}{
+			"5000/tcp": {},
+		},
+		Env: []string{
+			"REGISTRY_AUTH=htpasswd",
+			"REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm",
+			"REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd",
+			"REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt",
+			"REGISTRY_HTTP_TLS_KEY=/certs/domain.key",
+		},
+	}, &container.HostConfig{
+		Binds: []string{
+			filepath.Join(pwd, "testdata", "push", "auth") + ":/auth",
+			filepath.Join(pwd, "testdata", "push", "certs") + ":/certs",
+		},
+		PortBindings: map[nat.Port][]nat.PortBinding{
+			"5000/tcp": {
+				{
+					HostPort: "5000",
+				},
+			},
+		},
+	}, nil, nil, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	registryContainerID = resp2.ID
+
+	if err := cli.ContainerStart(context.Background(), registryContainerID, types.ContainerStartOptions{}); err != nil {
+		t.Fatal(err)
+	}
+
+	// Load .tar.gz filesystem into a local image
+	fileName := "vackup-pull-test-img.tar.gz"
+	absolutePath := filepath.Join(pwd, "testdata", "pull", fileName)
+	r, err := os.Open(absolutePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Close()
+
+	resp, err := cli.ImageLoad(context.Background(), r, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Push volume to registry.
+	// The registry container state may be "running" but not ready to receive requests when reaching this line.
+	err = retry(10, 1*time.Second, func() error {
+		pushResp, err := cli.ImagePush(context.Background(), imageID, dockertypes.ImagePushOptions{
+			RegistryAuth: encodedAuth,
+		})
+		if err != nil {
+			return err
+		}
+		defer pushResp.Close()
+
+		response, err := ioutil.ReadAll(pushResp)
+		if err != nil {
+			return err
+		}
+
+		t.Log(string(response))
+
+		if strings.Contains(string(response), "error") {
+			return err
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// The sleep is to ensure the image is present in the registry after the `ImagePush` operation.
+	time.Sleep(3 * time.Second)
+
+	_, err = cli.ImageRemove(context.Background(), imageID, types.ImageRemoveOptions{
+		Force: true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create empty volume where the content of the image pulled will be saved into
+	_, err = cli.VolumeCreate(context.Background(), volumetypes.VolumeCreateBody{
+		Driver: "local",
+		Name:   volume,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Pull volume from registry
+	err = h.PullVolume(c)
+
+	require.NoError(t, err)
+	require.Equal(t, http.StatusCreated, rec.Code)
+
+	// Check the content of the volume
+	m := backend.GetVolumesSize(c.Request().Context(), h.DockerClient, volume)
+	require.Equal(t, int64(16000), m[volume].Bytes)
+	require.Equal(t, "16.0 kB", m[volume].Human)
+}
+
+func TestPullVolumeUsingWrongAuthShouldFail(t *testing.T) {
+	var containerID string
+	var registryContainerID string
+	volume := "98fbf55c3ab45ea5bfc8ea8edf7f374b325e68eab947828aae5d6447df19ee3d"
+	registry := "localhost:5000" // or use docker.io to push it to DockerHub
+	imageID := registry + "/felipecruz/" + "vackup-pull-test-img"
+	cli := setupDockerClient(t)
+
+	defer func() {
+		_ = cli.ContainerRemove(context.Background(), registryContainerID, types.ContainerRemoveOptions{
+			Force: true,
+		})
+		_ = cli.ContainerRemove(context.Background(), containerID, types.ContainerRemoveOptions{
+			Force: true,
+		})
+		_ = cli.VolumeRemove(context.Background(), volume, true)
+
+		t.Logf("removing image %s", imageID)
+		if _, err := cli.ImageRemove(context.Background(), imageID, types.ImageRemoveOptions{
+			Force: true,
+		}); err != nil {
+			t.Log(err)
+		}
+	}()
+
+	auth := `{"username": "testuser", "password": "wrongpassword"}`
+	encodedAuth := base64.StdEncoding.EncodeToString([]byte(auth))
+
+	// Setup
+	e := echo.New()
+	requestJSON := fmt.Sprintf(`{"reference": "%s"}`, imageID)
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(requestJSON))
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("X-Registry-Auth", encodedAuth)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/volumes/:volume/pull")
+	c.SetParamNames("volume")
+	c.SetParamValues(volume)
+	h := New(c.Request().Context(), setupDockerClient(t))
+
+	// Run a local registry with auth
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp2, err := cli.ContainerCreate(context.Background(), &container.Config{
+		Image: "docker.io/library/registry:2",
+		ExposedPorts: map[nat.Port]struct{}{
+			"5000/tcp": {},
+		},
+		Env: []string{
+			"REGISTRY_AUTH=htpasswd",
+			"REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm",
+			"REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd",
+			"REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt",
+			"REGISTRY_HTTP_TLS_KEY=/certs/domain.key",
+		},
+	}, &container.HostConfig{
+		Binds: []string{
+			filepath.Join(pwd, "testdata", "push", "auth") + ":/auth",
+			filepath.Join(pwd, "testdata", "push", "certs") + ":/certs",
+		},
+		PortBindings: map[nat.Port][]nat.PortBinding{
+			"5000/tcp": {
+				{
+					HostPort: "5000",
+				},
+			},
+		},
+	}, nil, nil, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	registryContainerID = resp2.ID
+
+	if err := cli.ContainerStart(context.Background(), registryContainerID, types.ContainerStartOptions{}); err != nil {
+		t.Fatal(err)
+	}
+
+	// The registry container state may be "running" but not ready to receive requests when reaching this line.
+	err = retry(10, 1*time.Second, func() error {
+		req, err := http.NewRequest("GET", "https://localhost:5000/v2/_catalog", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		req.Header.Add("Authorization", "Basic "+basicAuth("testuser", "testpassword"))
+
+		httpClient := &http.Client{
+			Transport: &http.Transport{
+				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+			},
+		}
+		catalogResp, err := httpClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer catalogResp.Body.Close()
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Create empty volume where the content of the image pulled will be saved into
+	_, err = cli.VolumeCreate(context.Background(), volumetypes.VolumeCreateBody{
+		Driver: "local",
+		Name:   volume,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Pull volume from registry
+	err = h.PullVolume(c)
+
+	require.NoError(t, err)
+	require.Equal(t, http.StatusUnauthorized, rec.Code)
+
+	// Check the content of the volume
+	m := backend.GetVolumesSize(c.Request().Context(), h.DockerClient, volume)
+	require.Equal(t, int64(0), m[volume].Bytes)
+	require.Equal(t, "0 B", m[volume].Human)
 }
 
 func retry(attempts int, sleep time.Duration, f func() error) (err error) {
@@ -191,12 +465,3 @@ func retry(attempts int, sleep time.Duration, f func() error) (err error) {
 	}
 	return fmt.Errorf("after %d attempts, last error: %s", attempts, err)
 }
-
-// TODO:
-//func TestPullVolumeUsingCorrectAuth(t *testing.T) {
-//}
-//
-
-// TODO:
-//func TestPullVolumeUsingWrongAuthShouldFail(t *testing.T) {
-//}
