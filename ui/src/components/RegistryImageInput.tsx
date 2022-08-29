f96278ab5a64f31e5d16efd6ff5e32dc18891727
Felipe Cruz Martinez <15997951+felipecruz91@users.noreply.github.com>
2022-08-29 18:16:22 +0200

    Do not allow to push volumes larger than 10GB (#53)

--- ui/src/components/RegistryImageInput.tsx
+++ ui/src/components/RegistryImageInput.tsx
@@ -5,21 +5,31 @@ interface Props {
   value: string;
   setValue(newValue: string): void;
   setError(newValue: string): void;
+  volumeSize?: number;
 }
 export const RegistryImageInput = ({
   error,
   value,
   setValue,
   setError,
+  volumeSize,
 }: Props) => {
   const handleRegistryImageValidation = (newVal: string) => {
     if (!newVal) setError(null);
+    // If volume exceeds 10GB, we prevent users from pushing it to DockerHub
+    if(isDockerRegistry && sizeExceededMessage){
+      setError("Pushing volumes larger than 10GB are not supported at the moment through this extension.")
+      return
+    }
     if (!new RegExp(/(?:.*\/)([^:]+)(?::.+)?/gm).test(newVal)) {
       setError("Please specify at least <user>/<repo-name>:<tag>.");
     } else {
       setError(null);
     }
   };
+
+  const isDockerRegistry = (value.split("/")[0].startsWith("docker.io") || value.split("/").length == 2);
+  const sizeExceededMessage = volumeSize > 10 * 1000 * 1000 * 1000;
 
   return (
     <TextField
