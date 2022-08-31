be586c85af595d8e28a71634f88b08dc4d106110
Lucas Bernalte <lucas.bernalte@docker.com>
2022-08-31 09:29:22 +0200

    feat(lint): add prettier

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "plugin:react/jsx-runtime",
+    "plugin:prettier/recommended",
   ],
   overrides: [],
   parser: "@typescript-eslint/parser",
