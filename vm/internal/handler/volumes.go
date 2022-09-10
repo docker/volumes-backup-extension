fa80871be834ffe96f39496792a620f099f0d0a1
felipecruz91 <felipecruz91@hotmail.es>
2022-09-10 14:32:43 +0200

    Fix test

--- vm/internal/handler/volumes.go
+++ vm/internal/handler/volumes.go
@@ -38,69 +38,12 @@ func (h *Handler) Volumes(ctx echo.Context) error {
 
 	for _, vol := range v.Volumes {
 		res.data[vol.Name] = VolumeData{
-			Driver: vol.Driver,
-			//Size:       0,
-			//SizeHuman:  "",
-			//Containers: nil,
+			Driver:     vol.Driver,
+			Size:       -1,   // set to `-1` if the value is not available.
+			SizeHuman:  "-1", // set to `-1` if the value is not available.
+			Containers: nil,  // set to `nil` if the value is not available.
 		}
 	}
 
-	//var wg sync.WaitGroup
-	//// Calculating the volume size by spinning a container that execs "du " **per volume** is too time-consuming.
-	//// To reduce the time it takes, we get the volumes size by running only one container that execs "du"
-	//// into the /var/lib/docker/volumes inside the VM.
-	//volumesSize := backend.GetVolumesSize(ctx.Request().Context(), cli, "*")
-	//res.Lock()
-	//for k, v := range volumesSize {
-	//	entry, ok := res.data[k]
-	//	if !ok {
-	//		res.data[k] = VolumeData{
-	//			Size:      v.Bytes,
-	//			SizeHuman: v.Human,
-	//		}
-	//		continue
-	//	}
-	//	entry.Size = v.Bytes
-	//	entry.SizeHuman = v.Human
-	//	res.data[k] = entry
-	//}
-	//res.Unlock()
-	//
-	//for _, vol := range v.Volumes {
-	//	wg.Add(2)
-	//	go func(volumeName string) {
-	//		defer wg.Done()
-	//		driver := backend.GetVolumeDriver(context.Background(), cli, volumeName) // TODO: use request context
-	//		res.Lock()
-	//		defer res.Unlock()
-	//		entry, ok := res.data[volumeName]
-	//		if !ok {
-	//			res.data[volumeName] = VolumeData{
-	//				Driver: driver,
-	//			}
-	//			return
-	//		}
-	//		entry.Driver = driver
-	//		res.data[volumeName] = entry
-	//	}(vol.Name)
-	//
-	//	go func(volumeName string) {
-	//		defer wg.Done()
-	//		containers := backend.GetContainersForVolume(context.Background(), cli, volumeName) // TODO: use request context
-	//		res.Lock()
-	//		defer res.Unlock()
-	//		entry, ok := res.data[volumeName]
-	//		if !ok {
-	//			res.data[volumeName] = VolumeData{
-	//				Containers: containers,
-	//			}
-	//			return
-	//		}
-	//		entry.Containers = containers
-	//		res.data[volumeName] = entry
-	//	}(vol.Name)
-	//}
-	//
-	//wg.Wait()
 	return ctx.JSON(http.StatusOK, res.data)
 }
